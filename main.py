cf07a675872e9bfb3865f5a6aa0be823d07da21c
nanometer5088 <invalid-email@email.com>
2022-11-03 21:43:53 -0300

    PEP 8 compliance So zay don't curse at me because of wildcard imports

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from src.generic import *
-from src.encrypt import *
-from src.funcoes import *
-
-#print(senhas()[0])
-
-#frase = input("senha")
-#aha = codificar_senha(frase)
-#print(aha)
-
-#senhas_para_arquivo('k')
-#print(find('JqnPIZYP5+WTMI+tIAplfXhbY0tUd82xgV+wrss7ucbHumwI5bs5epKQ7SO7v6y5c3RzHNhF+0V+ivSbRgamqA=='))
+from src.funcoes import comparatd
 
 k = comparatd()
 if len(k) > 1:
